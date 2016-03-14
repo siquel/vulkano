1c54036ef8f18fcf64202cf89178fe5c6e74788d
siquel <squual@kahvipaussi.net>
2016-03-14 17:56:21 +0200

    Create TRACE

--- main.cpp
+++ main.cpp
@@ -106,6 +106,30 @@ const char* vkResultToString(const VkResult r) {
 	}
 #undef TOSTR
 }
+namespace detail {
+	inline int32_t vsnprintf(char* _str, size_t _count, const char* _format, va_list _argList) {
+		int32_t len = ::vsnprintf_s(_str, _count, size_t(-1), _format, _argList);
+		return -1 == len ? ::_vscprintf(_format, _argList) : len;
+	}
+}
+
+void trace(const char* path, uint16_t line, const char* fmt, ...) {
+	va_list args;
+	va_start(args, fmt);
+	char temp[8192];
+	char* out = temp;
+	int32_t len = detail::vsnprintf(out, sizeof(temp), fmt, args);
+	out[len] = '\0';
+	va_end(args);
+	std::cout << out;
+}
+
+#define _SIQ_TRACE(fmt, ...)                                               \
+	for(;;) {                                                              \
+		trace(__FILE__, uint16_t(__LINE__), fmt "\n", ##__VA_ARGS__);      \
+	break;}                                                                \
+
+#define SIQ_TRACE _SIQ_TRACE
 
 void openConsole() {
 	AllocConsole();
@@ -125,7 +149,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLin
 	VkInstance instance;
 	VkResult error; 
 	if ((error = createInstance(&instance))) {
-		std::cout << "Failed to create vulkan instance" << vkResultToString(error) << std::endl;
+		SIQ_TRACE("failed to create vulkan instance: %s", vkResultToString(error));
 		return 1;
 	}
 	/*
