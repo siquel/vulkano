a24b86be20742e7de678f204b7aa090c0a848ce8
siquel <squual@kahvipaussi.net>
2016-03-14 17:30:00 +0200

    Translate VkResult to const char*

--- main.cpp
+++ main.cpp
@@ -68,14 +68,63 @@ VkResult createInstance(VkInstance* to) {
 	return vkCreateInstance(&instanceInfo, nullptr, to);
 }
 
+void queryDevices(VkInstance instance) {
+	uint32_t gpuCount{ 0 };
+	VkResult error;
+	vkEnumeratePhysicalDevices(instance, &gpuCount, nullptr);
+}
+
+const char* vkResultToString(const VkResult r) {
+#define TOSTR(r) case VK_##r: return #r;
+	switch (r) {
+		TOSTR(SUCCESS);
+		TOSTR(NOT_READY);
+		TOSTR(TIMEOUT);
+		TOSTR(EVENT_SET);
+		TOSTR(EVENT_RESET);
+		TOSTR(INCOMPLETE);
+		TOSTR(ERROR_OUT_OF_HOST_MEMORY);
+		TOSTR(ERROR_OUT_OF_DEVICE_MEMORY);
+		TOSTR(ERROR_INITIALIZATION_FAILED);
+		TOSTR(ERROR_DEVICE_LOST);
+		TOSTR(ERROR_MEMORY_MAP_FAILED);
+		TOSTR(ERROR_LAYER_NOT_PRESENT);
+		TOSTR(ERROR_EXTENSION_NOT_PRESENT);
+		TOSTR(ERROR_FEATURE_NOT_PRESENT);
+		TOSTR(ERROR_INCOMPATIBLE_DRIVER);
+		TOSTR(ERROR_TOO_MANY_OBJECTS);
+		TOSTR(ERROR_FORMAT_NOT_SUPPORTED);
+		TOSTR(ERROR_SURFACE_LOST_KHR);
+		TOSTR(ERROR_NATIVE_WINDOW_IN_USE_KHR);
+		TOSTR(SUBOPTIMAL_KHR);
+		TOSTR(ERROR_OUT_OF_DATE_KHR);
+		TOSTR(ERROR_INCOMPATIBLE_DISPLAY_KHR);
+		TOSTR(ERROR_VALIDATION_FAILED_EXT);
+		TOSTR(ERROR_INVALID_SHADER_NV);
+	default:
+		return "UNKOWN_ERROR";
+	}
+}
+
+void openConsole() {
+	AllocConsole();
+	AttachConsole(GetCurrentProcessId());
+	// redirect to stdout 
+	freopen("CON", "w", stdout);
+	SetConsoleTitle(TEXT("topkek"));
+}
+
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {
+
+	openConsole();
+
 	window = std::make_unique<siq::Window>(1280, 720, hInstance, WndProc);
 	window->show();
 
 	VkInstance instance;
 	VkResult error; 
 	if ((error = createInstance(&instance))) {
-		std::cout << "Failed to create vulkan instance" << error << std::endl;
+		std::cout << "Failed to create vulkan instance" << vkResultToString(error) << std::endl;
 		return 1;
 	}
 	/*
